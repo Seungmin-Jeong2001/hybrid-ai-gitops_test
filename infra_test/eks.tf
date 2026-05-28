73966c4a2a3780495028aa737d4c489a302578c6
Jeong <mis084110@gmail.com>
2026-05-28 17:26:40 +0900

    imageupdater 기능 으로 변경

--- infra_test/eks.tf
+++ infra_test/eks.tf
@@ -10,6 +10,9 @@ module "eks" {
 
   cluster_endpoint_public_access = true
 
+  # OIDC 공급자 활성화 (IRSA 필수)
+  enable_irsa = true
+
   # 로그 그룹 충돌 방지: 테라폼이 직접 생성하지 않도록 설정
   create_cloudwatch_log_group = false
 
@@ -26,7 +29,7 @@ module "eks" {
       ami_type       = "AL2023_x86_64_STANDARD"
 
       # 비용 절감을 위해 스팟 인스턴스 유지
-      capacity_type = "SPOT"
+      capacity_type = SPOT
 
       # ECR 이미지 풀 권한을 명시적으로 추가
       iam_role_additional_policies = {
@@ -36,4 +39,24 @@ module "eks" {
   }
 
   enable_cluster_creator_admin_permissions = true
+}
+
+# 1. ArgoCD Image Updater 전용 IAM Role 생성 (IRSA)
+module "argocd_image_updater_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name = "argocd-image-updater-role"
+  
+  # Image Updater가 ECR의 태그 목록을 조회할 수 있는 권한 부여
+  role_policy_arns = {
+    policy = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  }
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["argocd:argocd-image-updater"]
+    }
+  }
 }
