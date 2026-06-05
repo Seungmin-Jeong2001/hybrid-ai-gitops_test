f1bd14e012102ab27ad8d9b5c25a3c07cde1dbc8
Jeong <mis084110@gmail.com>
2026-06-05 11:27:05 +0900

    ci 로 변경

--- infra_test/main.tf
+++ infra_test/main.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     aws        = { source = "hashicorp/aws", version = "~> 5.0" }
     local      = { source = "hashicorp/local", version = "~> 2.0" }
-    cloudflare = { source = "cloudflare/cloudflare", version = "~> 4.0" }
     random     = { source = "hashicorp/random", version = "~> 3.0" }
   }
 }
@@ -11,6 +10,3 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-provider "cloudflare" {
-  # API Token은 환경 변수 CLOUDFLARE_API_TOKEN으로 주입받는 것을 권장합니다.
-}
